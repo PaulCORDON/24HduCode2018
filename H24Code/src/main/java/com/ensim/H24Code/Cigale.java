a7ed8eded39df16e7244fe2540218737ad5a0c8d
Unknown <florentin.paillier@gmail.com>
2018-01-20 21:46:54 +0100

    beau parser

--- H24Code/src/main/java/com/ensim/H24Code/Cigale.java
+++ H24Code/src/main/java/com/ensim/H24Code/Cigale.java
@@ -1,7 +1,18 @@
 package com.ensim.H24Code;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.naming.CommunicationException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.squareup.okhttp.Response;
+
 public class Cigale {
-	Position positions[];
+	ArrayList<Position> track = new ArrayList<Position>();
+	ArrayList<String> idTrack = new ArrayList<String>();
 	
 	boolean estEnDehorsDeRoute() {
 		return false;
@@ -22,9 +33,33 @@ public class Cigale {
 	boolean refusPriorite() {
 		return false;
 	}
+	
+	public void parseTrack () throws IOException {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ComunicationServeur serveur = new ComunicationServeur();
+		
+		Response tracksAdverse = serveur.ListOtherTeamsTracks(serveur.Auth("ant1@mill.ant", "Vent").body().string().split(":\"")[1].split("\"}")[0]);
+	//	System.out.println(tracksAdverse.body().string());
+		
+		String jsonDoc = tracksAdverse.body().string();
+		System.out.println(jsonDoc);
+	
+	
+		JSONArray jsonarray = new JSONArray(jsonDoc);
+		for (int i = 0; i < jsonarray.length(); i++) {
+		    JSONObject jsonobject = jsonarray.getJSONObject(i);
+		    String id = jsonobject.getString("_id");
+		   System.out.println(id);
+		}
+	
+	
+	}
+	
+
+	public static void main(String[] args) throws IOException {
+		Cigale cigale = new Cigale ();
+	
+		cigale.parseTrack();
 
 	}
 
