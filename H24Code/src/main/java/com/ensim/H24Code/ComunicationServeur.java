c40107385c4954cc6e467177dc2be14feda33e97
Valentin LE MECHEC <valentin.lemechec@gmail.com>
2018-01-20 20:43:29 +0100

    Auto stash before merge of "master" and "origin/master"

--- H24Code/src/main/java/com/ensim/H24Code/ComunicationServeur.java
+++ H24Code/src/main/java/com/ensim/H24Code/ComunicationServeur.java
@@ -65,13 +65,13 @@ public class ComunicationServeur {
 
 	
 	
-	public Response showPosition() {
+	public Response showPosition(String id) {
 		OkHttpClient client = new OkHttpClient();
 		Request request = new Request.Builder()
 		  .url("https://f24h2018.herokuapp.com/api/positions/ChIJZ4ayW7yO4kcR3Jr7Wy_82Tk")
 		  .get()
 		  .addHeader("Content-Type", "application/json")
-		  .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTYzMmE4MWYzNmQyODcwODdhMTk4NzUiLCJyb2xlIjoiYW50IiwiaWF0IjoxNTE2NDU3ODAyLCJleHAiOjE1MTY0NzU4MDJ9.dYuP25Jd8tHmlJnKIOgtKrbqhfHoRo2EiX_ok_7Vlsk")
+		  .addHeader("Authorization", id)
 		  .addHeader("Cache-Control", "no-cache")
 		  .addHeader("Postman-Token", "e522ac4c-4ef2-5696-7939-a5149cce37f3")
 		  .build();
@@ -93,13 +93,13 @@ public class ComunicationServeur {
 	 * @param token
 	 * @return
 	 */
-	public Response describeSeed() {
+	public Response describeSeed(String id) {
 
 
 		Request request = new Request.Builder()
 		  .url("https://f24h2018.herokuapp.com/api/seeds/5a632a81f36d287087a19875")
 		  .get()
-		  .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTYzMmE4MWYzNmQyODcwODdhMTk4NzUiLCJyb2xlIjoiYW50IiwiaWF0IjoxNTE2NDU2NDg3LCJleHAiOjE1MTY0NzQ0ODd9.DELLy3Mm-lCiU34-ZuZE6YvpvI8eJV0_gWUUXUx2IHc")
+		  .addHeader("Authorization", "Bearer " +id)
 		  .addHeader("Cache-Control", "no-cache")
 		  .addHeader("Postman-Token", "75db8d96-8150-0420-8fd4-2b2aa0ddcf26")
 		  .build();
@@ -159,18 +159,15 @@ public Response ListMyTracks() {
 
 
 
-public Response ListOtherTeamsTracks() {
+public Response ListOtherTeamsTracks(String id) {
 	OkHttpClient client = new OkHttpClient();
 
-	MediaType mediaType = MediaType.parse("application/json");
-	RequestBody body = RequestBody.create(mediaType, "{\r\n    \"name\": \"petite balade\",\r\n    \"info\": \"vers l'infini et ...\",\r\n    \"startSeedId\": \"576900a615cf52a849374947\",\r\n    \"endSeedId\": \"186900a615ab52a849372541\"\r\n}");
 	Request request = new Request.Builder()
 	  .url("https://f24h2018.herokuapp.com/api/tracks/otherTeams")
-	  .post(body)
-	  .addHeader("Content-Type", "application/json")
-	  .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTYzMmE4MWYzNmQyODcwODdhMTk4NzUiLCJyb2xlIjoiYW50IiwiaWF0IjoxNTE2NDU3NzAyLCJleHAiOjE1MTY0NzU3MDJ9.LZ7l-88HzLot4gFyTpPVyo86mir6jjBQoAhp1rb5_vs")
+	  .get()
+	  .addHeader("Authorization","Bearer " +id)
 	  .addHeader("Cache-Control", "no-cache")
-	  .addHeader("Postman-Token", "6179edfd-4491-b803-c9be-88f538a99f46")
+	  .addHeader("Postman-Token", "884929ff-f652-bbf6-7c16-fc7faf48c420")
 	  .build();
 
 	Response response = null;
@@ -181,16 +178,15 @@ public Response ListOtherTeamsTracks() {
 		e.printStackTrace();
 	}
 	return response;
-	
 }
 
-public Response showATrack() {
+public Response showATrack(String id) {
 	OkHttpClient client = new OkHttpClient();
 
 	Request request = new Request.Builder()
 	  .url("https://f24h2018.herokuapp.com/api/tracks/5a632a81f36d287087a19875")
 	  .get()
-	  .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTYzMmE4MWYzNmQyODcwODdhMTk4NzUiLCJyb2xlIjoiYW50IiwiaWF0IjoxNTE2NDU3NzAyLCJleHAiOjE1MTY0NzU3MDJ9.LZ7l-88HzLot4gFyTpPVyo86mir6jjBQoAhp1rb5_vs")
+	  .addHeader("Authorization", id)
 	  .addHeader("Cache-Control", "no-cache")
 	  .addHeader("Postman-Token", "9fa953d7-79ca-3ef5-85b1-8eaeefbec103")
 	  .build();
@@ -206,13 +202,13 @@ public Response showATrack() {
 }
 
 
-public Response GetMyUserInfo() {
+public Response GetMyUserInfo(String id) {
 	OkHttpClient client = new OkHttpClient();
 	Request request = new Request.Builder()
 			  .url("https://f24h2018.herokuapp.com/api/users/me")
 			  .get()
 			  .addHeader("Content-Type", "application/json")
-			  .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTYzMmE4MWYzNmQyODcwODdhMTk4NzUiLCJyb2xlIjoiYW50IiwiaWF0IjoxNTE2NDU3ODAyLCJleHAiOjE1MTY0NzU4MDJ9.dYuP25Jd8tHmlJnKIOgtKrbqhfHoRo2EiX_ok_7Vlsk")
+			  .addHeader("Authorization", "Bearer "+id)
 			  .addHeader("Cache-Control", "no-cache")
 			  .addHeader("Postman-Token", "08e15559-5ba3-ca09-57d9-72bbd2cf8b9c")
 			  .build();
@@ -230,24 +226,38 @@ public Response GetMyUserInfo() {
 public static void main(String [] args) {
 	ComunicationServeur com=new ComunicationServeur();
 	System.out.println(com.Auth("ant1@mill.ant", "Vent").isSuccessful());
-	//System.out.println(com. CreatePositions("").isSuccessful());
-	System.out.println(com.showPosition().isSuccessful());
-	System.out.println(com.describeSeed().isSuccessful());
+	
+	String token=null;
+	
+	try {
+		System.out.println(com.Auth("ant1@mill.ant", "Vent").body().string().split(":\"")[1].split("\"}")[0]);
+		token=com.Auth("ant1@mill.ant", "Vent").body().string().split(":\"")[1].split("\"}")[0];
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	//System.out.println(com.CreatePositions("").isSuccessful());
+	System.out.println(com.showPosition(token).isSuccessful());
+	System.out.println(com.describeSeed(token).isSuccessful());
 	System.out.println(com.searchSeedAround().isSuccessful());
 	System.out.println(com.ListMyTracks().isSuccessful());
-	System.out.println(com.ListOtherTeamsTracks().isSuccessful());
-	System.out.println(com.showATrack().isSuccessful());
-	System.out.println(com.GetMyUserInfo().isSuccessful());
+	try {
+		System.out.println(com.ListOtherTeamsTracks(token).body().string());
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	System.out.println(com.showATrack(token).isSuccessful());
+	System.out.println(com.GetMyUserInfo(token).isSuccessful());
 
-	
 	/*System.out.println(com.searchSeedAround().toString().split("url=")[1].split("}")[0]);
 	try {
 		System.out.println(com.searchSeedAround().body().string());
 	} catch (IOException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
-	}
-	*/
+	}*/
+	
 	
 }
 
