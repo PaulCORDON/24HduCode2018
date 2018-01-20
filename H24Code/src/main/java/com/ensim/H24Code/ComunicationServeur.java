5bb0ae5b5c2465149170f2e91b06cf5c07f1c359
Valentin LE MECHEC <valentin.lemechec@gmail.com>
2018-01-20 14:01:59 +0100

    code authentification

--- H24Code/src/main/java/com/ensim/H24Code/ComunicationServeur.java
+++ H24Code/src/main/java/com/ensim/H24Code/ComunicationServeur.java
@@ -2,4 +2,17 @@ package com.ensim.H24Code;
 
 public class ComunicationServeur {
 
+	OkHttpClient client = new OkHttpClient();
+
+	MediaType mediaType = MediaType.parse("application/json");
+	RequestBody body = RequestBody.create(mediaType, "{\r\n    \"email\": \"test@example.com\",\r\n    \"password\": \"test\"\r\n}");
+	Request request = new Request.Builder()
+	  .url("https://f24h2018.herokuapp.com/auth/local")
+	  .post(body)
+	  .addHeader("Content-Type", "application/json")
+	  .addHeader("Cache-Control", "no-cache")
+	  .addHeader("Postman-Token", "3954a6ce-99b2-c2db-65ed-6c3b271a88e4")
+	  .build();
+
+	Response response = client.newCall(request).execute();
 }
