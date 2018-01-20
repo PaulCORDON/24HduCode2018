c690e94c3d7bf2c8ef336e3d70f4163f0d30f624
Valentin LE MECHEC <valentin.lemechec@gmail.com>
2018-01-20 14:18:44 +0100

    Auth

--- H24Code/src/main/java/com/ensim/H24Code/ComunicationServeur.java
+++ H24Code/src/main/java/com/ensim/H24Code/ComunicationServeur.java
@@ -4,15 +4,25 @@ public class ComunicationServeur {
 
 	OkHttpClient client = new OkHttpClient();
 
-	MediaType mediaType = MediaType.parse("application/json");
-	RequestBody body = RequestBody.create(mediaType, "{\r\n    \"email\": \"test@example.com\",\r\n    \"password\": \"test\"\r\n}");
-	Request request = new Request.Builder()
-	  .url("https://f24h2018.herokuapp.com/auth/local")
-	  .post(body)
-	  .addHeader("Content-Type", "application/json")
-	  .addHeader("Cache-Control", "no-cache")
-	  .addHeader("Postman-Token", "3954a6ce-99b2-c2db-65ed-6c3b271a88e4")
-	  .build();
+	public void Auth(String login, String mdp){
+		OkHttpClient client = new OkHttpClient();
 
-	Response response = client.newCall(request).execute();
+		MediaType mediaType = MediaType.parse("application/json");
+		RequestBody body = RequestBody.create(mediaType, "{\r\n    \"email\": \""+login+"\",\r\n    \"password\": \""+mdp+"\"\r\n}");
+		Request request = new Request.Builder()
+		  .url("https://f24h2018.herokuapp.com/auth/local")
+		  .post(body)
+		  .addHeader("Content-Type", "application/json")
+		  .addHeader("Cache-Control", "no-cache")
+		  .addHeader("Postman-Token", "a7c343d0-e873-5778-b49b-961daa54001d")
+		  .build();
+
+		Response response = client.newCall(request).execute();
+	}
+}
+
+
+public static void main(String [] args) {
+	
+	Auth("ant1@mill.ant", "Vent");
 }
