30e5c5f1dff0730fa91b74d08ac285f1fd4163b7
Unknown <florentin.paillier@gmail.com>
2018-01-20 16:39:22 +0100

    Ajout json pour le trajet

--- H24Code/src/main/java/com/ensim/H24Code/Chemin.java
+++ H24Code/src/main/java/com/ensim/H24Code/Chemin.java
@@ -1,6 +1,16 @@
 package com.ensim.H24Code;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.sun.javafx.css.CalculatedValue;
+
+import netscape.javascript.JSObject;
 
 public class Chemin extends ArrayList<Noeud> {
 	
@@ -8,5 +18,26 @@ public class Chemin extends ArrayList<Noeud> {
 	int temps;
 	
 	
-
+	public void calculItineraire () throws IOException {
+		
+		
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder()
+		  .url("https://roads.googleapis.com/v1/snapToRoads?path=47.984393,%200.236012|47.984946,%200.238951&interpolate=true&key=AIzaSyBdS5QGJC5iNRcKmAJxb3I7XF9TiX5Rj8Y")
+		  .get()
+		  .addHeader("Cache-Control", "no-cache")
+		  .addHeader("Postman-Token", "6ef08fe5-1f6e-939b-37ee-c7d12a755ea6")
+		  .build();
+		Response response = client.newCall(request).execute();
+		JSONObject jsonObj = new JSONObject(response.body().string());
+		System.out.println(jsonObj);
+	}
+	
+	
+	public static void main (String [] args ) throws IOException {
+		
+		Chemin c = new Chemin ();
+		c.calculItineraire();
+		
+	}
 }
