38decb00a6ef910200b8d0315cd39c86b52ee39c
Jaulin <alexandrejaulin@orange.fr>
2018-01-20 13:45:38 +0100

    position

--- H24Code/src/main/java/com/ensim/H24Code/Position.java
+++ H24Code/src/main/java/com/ensim/H24Code/Position.java
@@ -1,11 +1,24 @@
 package com.ensim.H24Code;
 
-import java.sql.Time;
+import java.util.GregorianCalendar;
 
 public class Position {
 	
 	double lat;
 	double lon;
-	Time temps;
+	GregorianCalendar temps;
+	
+	Position(double la, double lo, GregorianCalendar t){
+		lat=la;
+		lon=lo;
+		temps=t;
+	}
+	
+	double longueurEnKm(Position p1, Position p2) {
+		double deltaLatKm = (p1.lat-p2.lat)*110.574;
+		double deltaLonKm = (p1.lon-p2.lon)*111.320*Math.cos(p1.lat-p2.lat);
+		
+		return Math.sqrt(Math.pow(deltaLatKm, 2)+Math.pow(deltaLonKm, 2));	
+	}
 
 }
