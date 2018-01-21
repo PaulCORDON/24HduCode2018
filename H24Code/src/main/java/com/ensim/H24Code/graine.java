2d07e569d60ba2af641ac6b7faa98750be4b1ac0
Paul <paul.cordon@laposte.net>
2018-01-21 09:18:25 +0100

    Auto stash before merge of "master" and "24hCode/master"
    Ajout commentaires graines et graine

--- H24Code/src/main/java/com/ensim/H24Code/graine.java
+++ H24Code/src/main/java/com/ensim/H24Code/graine.java
@@ -1,5 +1,9 @@
 package com.ensim.H24Code;
-
+/**
+ * Structure d'une graine
+ * @author paul cordon
+ *
+ */
 public class graine {
 	double lat;
 	double lon;
--- H24Code/src/main/java/com/ensim/H24Code/graines.java
+++ H24Code/src/main/java/com/ensim/H24Code/graines.java
@@ -7,19 +7,38 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 
-
+/**
+ * Class servant à parser le resultat de la methode searchSeedAround de la class CommunicationServeur pour remplir son ArrayList de graine
+ * @author paul cordon
+ *
+ */
 public class graines {
 	ArrayList<graine> LesGrainesDeLaCarte=new ArrayList<graine>();
 	public graines(String token) {
 		ComunicationServeur com=new ComunicationServeur();
-		try {
-			String s =com.searchSeedAround(token).body().string();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			String jsonDoc = null;
+			try {
+				jsonDoc = com.searchSeedAround(token).body().string();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
+			System.out.println(jsonDoc);		
+		
+			JSONArray jsonarray = new JSONArray(jsonDoc);
+			for (int i = 0; i < jsonarray.length(); i++) {
+			    JSONObject jsonobject = jsonarray.getJSONObject(i);
+			    
+			    String id=null;
+			    if((id=jsonobject.getString("_id"))!= null) {
+			    	this.LesGrainesDeLaCarte.add(new graine());
+					LesGrainesDeLaCarte.get(LesGrainesDeLaCarte.size()-1).id=id;
+			    }
+				
 
-			
+			    
+			   System.out.println(id);
+			}
 		
 	}
 }
