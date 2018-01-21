6bd609b74f21c296c35a3fbdcf41db7530e8264d
Valentin LE MECHEC <valentin.lemechec@gmail.com>
2018-01-21 05:20:56 +0100

    debut app

--- H24Code/src/main/java/com/ensim/H24Code/App.java
+++ H24Code/src/main/java/com/ensim/H24Code/App.java
@@ -1,5 +1,7 @@
 package com.ensim.H24Code;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -8,10 +10,54 @@ public class App
 {
     public static void main( String[] args )
     {
-    	/* pour mon commit */ 
-    	int flo = 27;
-    	int balou= 10000;
-        System.out.println( "Hello World! lol" );
+    	ComunicationServeur com=new ComunicationServeur();
+    	
+    	Cigale cigale= new Cigale();
+    	String tokenCigale=null;
+    	try {
+			tokenCigale=com.Auth("cicada@mill.ant", "Vent").body().string().split(":\"")[1].split("\"}")[0];
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	Fourmi f1= new Fourmi();
+    	String token1=null;
+		try {
+			token1=com.Auth("ant1@mill.ant", "Vent").body().string().split(":\"")[1].split("\"}")[0];
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Fourmi f2= new Fourmi();
+    	String token2=null;
+		try {
+			token2=com.Auth("ant2@mill.ant", "Vent").body().string().split(":\"")[1].split("\"}")[0];
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Fourmi f3= new Fourmi();
+    	String token3=null;
+		try {
+			token3=com.Auth("an31@mill.ant", "Vent").body().string().split(":\"")[1].split("\"}")[0];
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println("search seed around: " +com.searchSeedAround(token1).body().string());
+			System.out.println("search seed around: " +com.searchSeedAround(token2).body().string());
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
         
         
     }
