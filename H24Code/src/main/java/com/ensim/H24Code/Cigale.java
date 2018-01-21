33368021fffb9b424d26798f714c4ad60973bb73
Valentin LE MECHEC <valentin.lemechec@gmail.com>
2018-01-21 09:16:11 +0100

    petit truc

--- H24Code/src/main/java/com/ensim/H24Code/Cigale.java
+++ H24Code/src/main/java/com/ensim/H24Code/Cigale.java
@@ -38,7 +38,7 @@ public class Cigale {
 				v = distanceEntreDeuxPts*3.6;
 				idTrack = track.get(8).id;
 				idPos = p.id;
-				boolean ok = serveur.AnalyseVitesseExesive(serveur.Auth("ant1@mill.ant", "Vent").body().string().split(":\"")[1].split("\"}")[0], "5a6350688fb12f001481b340", idPos, 50, v).isSuccessful();
+				boolean ok = serveur.AnalyseVitesseExesive(serveur.Auth("ant1@mill.ant", "Vent").body().string().split(":\"")[1].split("\"}")[0], "5a63e834df71750014294e8c", idPos, 50, v).isSuccessful();
 				System.out.println(ok);
 				return false;
 			}
